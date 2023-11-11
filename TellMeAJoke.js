8c2a9bfbdbded93c0022685224b12184447b74fe
Anuj123verma <anuj17026@iiitd.ac.in>
2023-11-11 23:35:54 +0530

    tell me a joke

---
+++ TellMeAJoke.js
@@ -0,0 +1,342 @@
+let jokes = [
+    {
+      "category": "Programming",
+      "type": "single",
+      "joke": "// This line doesn't actually do anything, but the code stops working when I delete it.",
+      "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+      },
+      "id": 12,
+      "safe": true,
+      "lang": "en"
+    },
+    {
+      "category": "Programming",
+      "type": "single",
+      "joke": "Eight bytes walk into a bar.\nThe bartender asks, \"Can I get you anything?\"\n\"Yeah,\" reply the bytes.\n\"Make us a double.\"",
+      "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+      },
+      "id": 34,
+      "safe": true,
+      "lang": "en"
+    },
+    {
+      "category": "Programming",
+      "type": "single",
+      "joke": "I have a joke about Stack Overflow, but you would say it's a duplicate.",
+      "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+      },
+      "safe": true,
+      "id": 301,
+      "lang": "en"
+    },
+    {
+      "category": "Programming",
+      "type": "single",
+      "joke": "If Bill Gates had a dime for every time Windows crashed ... Oh wait, he does.",
+      "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+      },
+      "id": 22,
+      "safe": true,
+      "lang": "en"
+    },
+    {
+      "category": "Programming",
+      "type": "twopart",
+      "setup": "Why did the Python programmer not respond to the foreign mails he got?",
+      "delivery": "Because his interpreter was busy collecting garbage.",
+      "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+      },
+      "id": 15,
+      "safe": true,
+      "lang": "en"
+    },
+    {
+      "category": "Programming",
+      "type": "single",
+      "joke": "Debugging is like being the detective in a crime movie where you're also the murderer at the same time.",
+      "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+      },
+      "id": 42,
+      "safe": true,
+      "lang": "en"
+    },
+    {
+      "category": "Programming",
+      "type": "single",
+      "joke": "\"We messed up the keming again guys.\"",
+      "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+      },
+      "id": 20,
+      "safe": true,
+      "lang": "en"
+    },
+    {
+      "category": "Programming",
+      "type": "single",
+      "joke": "Java is like Alzheimer's, it starts off slow, but eventually, your memory is gone.",
+      "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+      },
+      "id": 27,
+      "safe": false,
+      "lang": "en"
+    },
+    {
+      "category": "Programming",
+      "type": "single",
+      "joke": "The generation of random numbers is too important to be left to chance.",
+      "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+      },
+      "id": 39,
+      "safe": true,
+      "lang": "en"
+    },
+    {
+      "category": "Programming",
+      "type": "single",
+      "joke": "How do you tell HTML from HTML5?\n- Try it out in Internet Explorer\n- Did it work?\n- No?\n- It's HTML5.",
+      "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+      },
+      "id": 43,
+      "safe": true,
+      "lang": "en"
+    },
+    {
+      "category": "Christmas",
+      "type": "twopart",
+      "setup": "Who hides in the bakery at Christmas?",
+      "delivery": "A mince spy!",
+      "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+      },
+      "id": 250,
+      "safe": true,
+      "lang": "en"
+    },
+    {
+      "category": "Christmas",
+      "type": "twopart",
+      "setup": "Whats the Grinchs least favorite band?",
+      "delivery": "The Who.",
+      "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+      },
+      "safe": true,
+      "id": 293,
+      "lang": "en"
+    },
+    {
+      "category": "Christmas",
+      "type": "twopart",
+      "setup": "Who is Santa's favourite singer?",
+      "delivery": "Elf-is Presley!",
+      "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+      },
+      "id": 246,
+      "safe": true,
+      "lang": "en"
+    },
+    {
+      "category": "Christmas",
+      "type": "twopart",
+      "setup": "What do elves post on Social Media?",
+      "delivery": "Elf-ies!",
+      "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+      },
+      "id": 249,
+      "safe": true,
+      "lang": "en"
+    },
+    {
+      "category": "Christmas",
+      "type": "twopart",
+      "setup": "How will Christmas dinner be different after Brexit?",
+      "delivery": "No Brussels!",
+      "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": true,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+      },
+      "id": 251,
+      "safe": false,
+      "lang": "en"
+    },
+    {
+      "category": "Christmas",
+      "type": "twopart",
+      "setup": "Why does Santa go down the chimney?",
+      "delivery": "Because it soots him!",
+      "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+      },
+      "id": 253,
+      "safe": true,
+      "lang": "en"
+    },
+    {
+      "category": "Christmas",
+      "type": "twopart",
+      "setup": "What kind of motorbike does Santa ride?",
+      "delivery": "A Holly Davidson!",
+      "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+      },
+      "id": 244,
+      "safe": true,
+      "lang": "en"
+    },
+    {
+      "category": "Christmas",
+      "type": "twopart",
+      "setup": "Why couldn't the skeleton go to the Christmas party?",
+      "delivery": "Because he had no body to go with!",
+      "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+      },
+      "id": 252,
+      "safe": true,
+      "lang": "en"
+    },
+    {
+      "category": "Christmas",
+      "type": "twopart",
+      "setup": "What's Santa's favourite type of music?",
+      "delivery": "Wrap!",
+      "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+      },
+      "id": 247,
+      "safe": true,
+      "lang": "en"
+    },
+    {
+      "category": "Christmas",
+      "type": "twopart",
+      "setup": "Why does Santa have three gardens?",
+      "delivery": "So he can 'ho ho ho'!",
+      "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+      },
+      "id": 242,
+      "safe": true,
+      "lang": "en"
+    }
+  ]
+  
+  let index = Math.floor(Math.random()*jokes.length)
+  if(jokes[index].type=="single"){
+    joke.innerHTML = jokes[index].joke
+  }
+  else{
+    joke.innerHTML = jokes[index].setup + jokes[index].delivery
+  }
+  
